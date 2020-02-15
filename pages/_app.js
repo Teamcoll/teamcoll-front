d7f1252741aac93f86a25b18ac4b3586e45252bd
eurobin4321 <eurobin4321@gmail.com>
2020-02-15 18:11:32 +0900

    FEAT normalize css

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
-import { DefaultLayout } from '../components/Layout';
+import { DefaultLayout } from '~/components/Layout';
 
 export default function Teamcoll({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
 				<title>Teamcoll</title>
+				<link rel="stylesheet" href="reset/normalize.css" />
 			</Head>
 			<DefaultLayout>
 				<Component {...pageProps} />
