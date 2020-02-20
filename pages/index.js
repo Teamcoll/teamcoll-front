5ef4fd265177a22b1f834babe141e68a19d5b09c
Hong Jin Baek <jjam100@HONG.localdomain>
2020-02-20 15:24:20 +0900

    FIX delete loremipsum

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
 import styled from 'styled-components';
-import { loremipsum } from 'Component/map';
-const Header = styled.h2`
-	margin: 0px;
-	font-size: 2em;
-`;
 
 const Index = () => {
 	return (
 		<>
-			<Header>Index Page</Header>
-			<p>{loremipsum}</p>
-			<p>{loremipsum}</p>
-			<p>{loremipsum}</p>
-			<p>{loremipsum}</p>
+
 		</>
 	);
 };
